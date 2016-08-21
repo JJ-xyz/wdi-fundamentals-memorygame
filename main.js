e8ef887f69697bd63701d47e7031df9bf1e8b2e7
JJ xyz <jjxyz@JJs-MacBook-Pro.local>
2016-08-21 19:59:30 -0400

    create cards thru a loop

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 console.log("JS file is connected to HTML! Woo!")
+// --- Define the Game-Board as variable ---
+var gameboard = document.getElementById('game-board');
+
+// --- Function thet Create Board of cards ---
+var createBoard = function() {
+	// --- Loop to create the cards ---
+	for (var i=0; i<4; i++) {
+		var nuCard = document.createElement('div');
+		nuCard.className = 'card';
+		gameboard.appendChild(nuCard);
+	}
+}
+
+// Execute the create board
+createBoard();
+
+
+
+
+
 /* Initial Card Values */
 var cardOne = "king";
 var cardTwo = "king";
